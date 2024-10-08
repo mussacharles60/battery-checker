2b574b29d093a31046b408e68d1badbf3df49c44
Mussa Charles <mussacharles60@gmail.com>
2024-10-08 19:14:13 +0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,9 +9,28 @@ const BatteryStatus = require('./lib');
 
 const appID = 'Myssa - Battery Checker';
 const icon = path.join(__dirname, '../logo-round-192.png');
+const notificationID = 23;
 
 let interval = 60;
+let lower = 20;
+let higher = 100;
 let timer = null;
+
+// get arguments from the command line
+// const args = process.argv.slice(2); // Remove the first two elements (node executable and script filename)
+const args = process.argv;
+
+// Iterate through the arguments to retrieve variables
+for (let i = 0; i < args.length; i++) {
+  if (args[i] === '--interval' && i + 1 < args.length) {
+    // Remove double-quote characters from the value
+    interval = Number(args[i + 1].replace(/"/g, ''));
+  } else if (args[i] === '--lower' && i + 1 < args.length) {
+    lower = Number(args[i + 1].replace(/"/g, ''));
+  } else if (args[i] === '--higher' && i + 1 < args.length) {
+    higher = Number(args[i + 1].replace(/"/g, ''));
+  }
+}
 
 const speak = async (text) => {
   return new Promise((resolve, reject) => {
@@ -31,13 +50,18 @@ const checkBattery = async () => {
     const charging = batteryStatus.charging;
     const batteryPercentage = Math.round(level * 100);
 
-    console.log(`[battery-checker]: Battery: ${batteryPercentage}% ${charging ? 'charging' : 'not charging'}`);
+    console.log(
+      `[battery-checker]: Battery: ${batteryPercentage}% ${
+        charging ? 'charging' : 'not charging'
+      }`
+    );
 
-    if (charging && batteryPercentage >= 100) {
+    if (charging && batteryPercentage >= higher) {
       interval = 10;
       startTimer();
 
       notifier.notify({
+        id: notificationID,
         title: 'Battery Full',
         message: 'Your battery is fully charged. Please unplug the adapter.',
         sound: true,
@@ -56,20 +80,23 @@ const checkBattery = async () => {
       } catch (error) {
         console.error('[battery-checker]: speak: catch: error:', error);
       }
-    } else if (!charging && batteryPercentage <= 20) {
+    } else if (!charging && batteryPercentage <= lower) {
       interval = 10;
       startTimer();
 
       notifier.notify({
+        id: notificationID,
         title: 'Battery Low',
-        message: 'Your battery is below 20%. Please plug in the adapter.',
+        message: `Your battery is below ${lower}%. Please plug in the adapter.`,
         sound: true,
         appID,
         icon,
       });
 
       try {
-        await speak('Your battery is below 20%. Please plug in the adapter.');
+        await speak(
+          `Your battery is below ${lower}%. Please plug in the adapter.`
+        );
       } catch (error) {
         console.error('[battery-checker]: speak: catch: error:', error);
       }
